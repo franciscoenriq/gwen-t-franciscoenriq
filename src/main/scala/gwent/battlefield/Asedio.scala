3030aa2028bbe2cb69d39abc72915343d270ff45
franciscoenriq <fr.almeida.d@gmail.com>
2023-05-15 12:47:57 -0400

    Añadiendo clases del tablero
    Se crean la composición de los objetos de tipo tablero. De esta manera asedio, clima , cuerpocuerpo y distancia serán hijos de abstractTable

---
+++ src/main/scala/gwent/battlefield/Asedio.scala
@@ -0,0 +1,6 @@
+package cl.uchile.dcc
+package gwent.battlefield
+
+class Asedio extends AbstractTable {
+
+}
---
+++ src/main/scala/gwent/battlefield/Distancia.scala
@@ -0,0 +1,6 @@
+package cl.uchile.dcc
+package gwent.battlefield
+
+class Distancia extends AbstractTable {
+
+}
