107c5052757769bedddb79caaa536b4f03843dcd
franciscoenriq <fr.almeida.d@gmail.com>
2023-07-16 16:47:46 -0400

    creando estados :) atrasadito jejeje

---
+++ src/main/scala/gwent/controller/GameController.scala
@@ -0,0 +1,17 @@
+package cl.uchile.dcc
+package gwent.controller
+
+import scala.collection.mutable.ArrayBuffer
+import gwent.cards.Cards
+
+class GameController(private var deckOfCardsPlayer:ArrayBuffer[Cards],private var deckOfCardsCPU:ArrayBuffer[Cards]){
+   private var gameState :State = new GameInitialization()
+
+
+
+
+
+
+
+
+}
